8c74da7dd930016761149d9a364a8fe40de789e8
K-ai30 <artpirate11@aim.com>
2020-01-16 06:27:54 -0500

    initial attempt at jquery and ajax code

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+//  Identify the variables needed to target
+var resultCard = document.querySelector(".card");
+var searchBtn = document.querySelector("#search");
+var userEntry = document.querySelector(input);
+
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 
+    + "b9da2b7e40a98e59cb40534717905908";
+
+    $("#search").on('click', function(){
+
+    // Remove elements with hide
+    resultCard.classList.remove("hide");
+    searchBtn.classList.remove("hide");
+    userEntry.
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);
+    }
