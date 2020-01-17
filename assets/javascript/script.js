14643fc7cfd16f47f25ad51ca14bd4f34b331132
K-ai30 <artpirate11@aim.com>
2020-01-16 20:27:41 -0500

    added git ignore file to hide API keys

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,12 +5,15 @@ var searchBtn = document.querySelector("#search");
 var userEntry = document.querySelector(input);
 
 
+
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 
     + "b9da2b7e40a98e59cb40534717905908";
 
     $("#search").on('click', function(){
 
     // Remove elements with hide
+    document.getElementById().hidden = false;
+    document,getElementById("cardResults").hidden = true;
     resultCard.classList.remove("hide");
     searchBtn.classList.remove("hide");
     userEntry.
