7bbe89d08242fd8dfc2077ec82bfdd3964fa962c
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-01-18 10:33:34 -0500

    updates script.js

--- script.js
+++ script.js
@@ -1,101 +1,71 @@
 var latitude = 0;
 var longitude = 0;
-var sidePanelDiv=$(".sidepanel");
+
+
+
 
 // check if the browser support the getlocation API
 function supportRequest() {
   if (navigator.getLocation) {
+    
     navigator.geolocation.getCurrentPosition(getPosition);
-  }
-  else {
+  }else {
     alert("Geolocation is not supported by this browser !");
   }
 }
 
+function getPosition(success){
+  console.log(' getposition function')
+
+  latitude =  success.coords.latitude;
+  longitude = success.coords.longitude;
+  console.log('right after update', latitude, longitude)
+  
+  console.log(success);
+
+weather()
+
+
+}
 navigator.geolocation.getCurrentPosition(getPosition);
 
 
-// getting current location from navigator API
-function getPosition(position) {
-  console.log("this is position:", position);
-  console.log("this is latitude:", position.coords.latitude);
-  console.log("this is longitude", position.coords.longitude);
-  console.log();
-  latitude =  position.coords.latitude;
-  longitude = position.coords.longitude;
-  
-  console.log("this is latitude:",latitude);
-  console.log("this is longitude",longitude);
+
+function weather() {
+  console.log('weather function user location', latitude, longitude)
+
+  var urlBase ="https://api.openweathermap.org/data/2.5/weather?appid=b650042e3a82aa70290734a60a8cb3e3&lat="+latitude+"&lon="+longitude+"&units=imperial";
+                
+   
+
+  console.log(urlBase);
+
+  $.ajax({
+    url: urlBase,
+    type:"GET",
+    
+    success:function(wetherInfo){
+      console.log(wetherInfo);
+
+         $("#location").html("Location:")
+         $("#locationName").html(wetherInfo.name +  `<img src='http://openweathermap.org/img/w/${wetherInfo.weather[0].icon}.png'>`);
+         $("#tempature").html("Tempature: "+ wetherInfo.main.temp+" &#8457");
+         $("#humidity").html("Humidity: "+JSON.stringify(wetherInfo.main.humidity)+"%");
+         $("#wind").html("Wind speed : "+JSON.stringify(wetherInfo.wind.speed)+" m/s");
+
+        // $(".uvIndex").html("UVIndex: ");
+
+       
+
+
+    }
+});
+
+
 }
 
-function weatherSidePanel(){
-
-    $.ajax({
-        url:"https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=b650042e3a82aa70290734a60a8cb3e3=imperial",
-        type:"GET",
-        
-        success:function(wetherInfo){
-            console.log(wetherInfo);
-
-
-
-
-            $("<p>").html("");
-            sidePanelDiv.appended("<p>");
 
 
 
 
 
-            //  $(".cityName").html("Location: "+ wetherInfo.name +  `<img src='http://openweathermap.org/img/w/${wetherInfo.weather[0].icon}.png'>` + " Date: ");
-            // $(".weather").html(" Description : "+wetherInfo.weather[0].description);
-            // $(".tempature").html("Tempature: "+ wetherInfo.main.temp+"&#8457");
-            // $(".humidity").html("Humidity: "+JSON.stringify(wetherInfo.main.humidity)+" %");
-            // becuse speed object has number in it we need to convert that with JSON.stringfy to string
-            // $(".wind").html("Wind speed : "+JSON.stringify(wetherInfo.wind.speed)+"m/s");
-            // $(".uvIndex").html("UVIndex: ");
-    
-           
-    
-    
-        }
-    });
-    
-
-
-
-
-
-
-
-
-
-};
-weatherSidePanel();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(".sidepanel").html();
