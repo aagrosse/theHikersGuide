ffbcb1f85aae1e3dc3b287a073a4a591e9b06ff7
K-ai30 <artpirate11@aim.com>
2020-01-20 01:35:42 -0500

    solved issue with trails and city search formatting

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# theHikersGuide
+# The Hikers Guide
+
+### Description
+This is a hiker's trail application created using the Hiking Project Data API. This was a collabortive project, and, therefore, a huge learning experience. We went into this project with the intent to create a mobile friendly application that is interactive and includes two server-side APIs and one Third-Party API.  
+
+### Installation
+Clone to your local machine and open in your favorite code editor to check it out! Or, visit the deployed version at: 
+
+### Technologies Utilized
+* HTML5
+* CSS3
+* Materialize
+* JavaScript
+* jQuery
+* AJAX
+
+### Notes
+We started building this application in an effort to achieve a minimum viable product (MVP). To that end, we hope to achieve some future dated development ideas that will enhance the user engagement with the application.
+
+### Contributors
+
+The original contributors to this project are the following full stack developers:
+
+- Alex Grosse
+- Alia Kyle Perry
+- Arvin Mahmoudi
+
+As such, for major changes, please open an issue first to discuss what you would like to change. Changes will not be made without the consent of this development team.
+
+Copyright (c) [2020] [The Proud Rabbits]
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,107 +1,98 @@
 
 $(document).ready(function() {
+    pageLoad()
 
-
-console.log("hello")
-
-
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 
-    + "b9da2b7e40a98e59cb40534717905908";
-
-$('#modal1').modal();
-
-
-    // // Remove elements with hide
-    // document.getElementById(".row").hidden = false;
-    // document,getElementById("cardResults").hidden = true;
-    // resultCard.classList.remove("hide");
-    // searchBtn.classList.remove("hide");
-    // // userEntry.s
+    $('#modal1').modal();
     
-
-
-function getTrails (lat, lon) {
-    var trailKey = "200668995-d0d69e4094ff3a415bc5f83a7340a09a";
-    var dist = 10;
-    var res = 16;
-    var trailQueryURL = "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=" + dist + "&maxResults=" + res + "&key=" + trailKey;
- 
-console.log(trailQueryURL)
-
-$.ajax({
-        url: trailQueryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-
-        for (var i = 0; i < 16; i++){
-            $("#image-" + i).attr("src", response.trails[i].imgMedium)
-            $(".card-title-" + i).text(response.trails[i].name)
-
-        } 
-
-      
-console.log(response.trails[0].name)
-
-     });
-}
-
-
-getTrails(33.763406, -84.395075);
-
-
-
-// check if the browser support the getlocation API
-function supportRequest() {
-  if (navigator.getLocation) {
-    navigator.geolocation.getCurrentPosition(getPosition);
-  }
-  else {
-    alert("Geolocation is not supported by this browser !");
-  }
-}
-navigator.geolocation.getCurrentPosition(getPosition);
-// getting current location from navigator API
-function getPosition(position) {
-  console.log("this is position:", position);
-  console.log("this is latitude:", position.coords.latitude);
-  console.log("this is longitude", position.coords.longitude);
-  latitude =  position.coords.latitude;
-  longitude = position.coords.longitude;
-  console.log("this is latitude:",latitude);
-  console.log("this is longitude",longitude);
-}
-
-
-
-
-
-
-
-
-
-
-
-//  Identify the variables needed to target
-// var resultCard = document.querySelector(".card");
-// var searchBtn = document.querySelector("#search");
-// // var userEntry = document.querySelector(input);
-
-
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 
-//     + "b9da2b7e40a98e59cb40534717905908";
-
-//     $("#search").on('click', function(){
-
-//     // Remove elements with hide
-//     resultCard.classList.remove("hide");
-//     searchBtn.classList.remove("hide");
     
-
-
+    $(".btn-floating").on("click", function() {
+     $('#modal1').modal('open');
+    });
     
-
-
+    $("#search").on("click", function(){
+      //search button click event that starts all the fun
+      var cityName= $("#input").val();
+      geoCode (cityName);
     });
 
+
+
+    function geoCode (city) {
+        var geoKey = "f51b969f42a69a";
+        var geoURL = "https://us1.locationiq.com/v1/search.php?key=" + geoKey +"&q=" + city + "&format=json";
+        $.ajax({
+          url: geoURL,
+          method: "GET"
+      }).then(function(response) {
+          console.log(response);
+          lat = response[0].lat;
+          lon = response[0].lon;
+          getTrails (lat, lon);
+        });
+      }
+
+
+
+      geoCode("atlanta")
+
+    // Remove elements with hide
+    function pageLoad() {
+            // var hidden = document.querySelector(".row");
+            // hidden.classList.remove("hide")
+            $(".row").hide();
+        }
+
+    // This code will show the results once the user inputs a city
+    // function showResults() {
+
+    // }
+
+    function getTrails (lat, lon) {
+        var trailKey = "200668995-d0d69e4094ff3a415bc5f83a7340a09a";
+        var dist = 10;
+        var res = 16;
+        var trailQueryURL = "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=" + dist + "&maxResults=" + res + "&key=" + trailKey;
+     
+    // console.log(trailQueryURL)
+    
+    $.ajax({
+            url: trailQueryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+    
+            for (var i = 0; i < 16; i++){
+                $("#image-" + i).attr("src", response.trails[i].imgMedium)
+                $(".card-title-" + i).text(response.trails[i].name)
+    
+            } 
+    
+          
+    // console.log(response.trails[0].name)
+    
+         });
+    }
+    
+    
+    getTrails(33.763406, -84.395075);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    
+    
+        });
     
